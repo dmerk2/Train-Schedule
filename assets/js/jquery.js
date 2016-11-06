9606272e866de85f4a132c6fdeaa617ede997335
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-06 14:46:48 -0500

    switched to nextarrival and frequency

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,4 +1,4 @@
-	//creating firebase connection
+//creating firebase connection
 var dataRef = new Firebase("https://sanfrantrainschedule.firebaseio.com/");	
 
 $(document).ready(function(){
@@ -8,15 +8,15 @@ $(document).ready(function(){
 	//captures what the user inputs and stores into variables
 		var train = $('#trainInput').val().trim();
 		var destination = $('#destinationInput').val().trim();
-		var militaryTime = $('#militaryTimeInput').val().trim();
-		var nextTrain = $('#nextTrainInput').val().trim();
+		var nextArrival = moment($("#nextArrivalInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+		var frequency = $('#frequencyInput').val().trim();
 
 		// Code for handling the push
 		dataRef.push({
 			train: train,
 			destination: destination,
-			militaryTime: militaryTime,
-			nextTrain: nextTrain
+			nextArrival: nextArrival,
+			frequency: frequency
 		});
 
 		// Don't refresh the page!
@@ -30,42 +30,56 @@ $(document).ready(function(){
 		// Log everything that's coming out of snapshot
 		console.log(childSnapshot.val().train);
 		console.log(childSnapshot.val().destination);
-		console.log(childSnapshot.val().militaryTime);
-		console.log(childSnapshot.val().nextTrain)
+		console.log(childSnapshot.val().nextArrival);
+		console.log(childSnapshot.val().frequency)
 
 
 	// Creating variables to append user info
 	var tName = childSnapshot.val().train;
 	var tDestination = childSnapshot.val().destination;
-	var tFrequency = childSnapshot.val().militaryTime;
-	var tNextTrain = childSnapshot.val().nextTrain;
+	var tNextArrival = childSnapshot.val().nextArrival;
+	var tFrequency = childSnapshot.val().frequency;
 
 		// Appends user info to table
-		$(".table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tNextTrain + "</td>");
+		$(".table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tNextArrival + "</td><td>" + tFrequency + "</td></tr>");
+		// $(".table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tNextArrival + "</td><td>" + tFrequency + "</td><td>" + tMinutes + "</td></tr>");
 
 		// Clears the HTML in bottom form
 		$("#trainInput").val("");
 		$("#destinationInput").val("");
-		$("#militaryTimeInput").val("");
-		$("#nextTrainInput").val("");
+		$("#nextArrivalInput").val("");
+		$("#frequencyInput").val("");
 
 		// ONLY WHEN I CLEAR FIREBASE.COM WILL THE TOP FORM EMPTY?!?!
 		// HOW DO I CLEAR THE HTML IN THE TOP FORM???
-		// $(".table > tbody").empty("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tNextTrain + "</td>");
+		// $(".table > tbody").empty("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tNextArrival + "</td><td>" + tFrequency + "</td>");
 
 		// $("<td>").val("");
 		// $(tDestination).val("");
+		// $(tNextArrival).val("");
 		// $(tFrequency).val("");
-		// $(tNextTrain).val("");
 
 		// Handle the errors
-	},
-		function(errorObject){
-		console.log("Errors handled: " + errorObject.code);
-	});
+	// });
+	// 	function(errorObject){
+	// 	console.log("Errors handled: " + errorObject.code);
+	// });
 
-	dataRef.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+	// dataRef.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+	// 	// Calculate the minutes until arrival using hardcore math
+	// 	// To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time and find the modulus between the difference and the frequency  
+	// 	var differenceTimes = moment().diff(moment.unix(tFirstTrain), "minutes");
+	// 	var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency ;
+	// 	var tMinutes = tFrequency - tRemainder;
 
+	// 	// To calculate the arrival time, add the tMinutes to the currrent time
+	// 	var tArrival = moment().add(tMinutes, "m").format("hh:mm A"); 
+	// 		console.log(tMinutes);
+	// 		console.log(tArrival);
+
+	// 		console.log(moment().format("hh:mm A"));
+	// 		console.log(tArrival);
+	// 		console.log(moment().format("X"));
 	});
 });
 
