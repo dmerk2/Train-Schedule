5c7c1e26f9f598adc5400c69f0d624bd6817b404
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-06 11:57:15 -0500

    trying to delete top table without needing to delete storage from firebase.com

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,61 +1,73 @@
-//creating firebase connection
+	//creating firebase connection
 var dataRef = new Firebase("https://sanfrantrainschedule.firebaseio.com/");	
 
-//captures when submit button is pushed
-$('#addTrain').on('click', function(){ 
+$(document).ready(function(){
+	//captures when submit button is pushed
+	$('#addTrain').on('click', function(){ 
 
-//captures what the user inputs and stores into variables
-	train = $('#trainInput').val().trim();
-	destination = $('#destinationInput').val().trim();
-	militaryTime = $('#militaryTimeInput').val().trim();
-	nextTrain = $('#nextTrainInput').val().trim();
+	//captures what the user inputs and stores into variables
+		var train = $('#trainInput').val().trim();
+		var destination = $('#destinationInput').val().trim();
+		var militaryTime = $('#militaryTimeInput').val().trim();
+		var nextTrain = $('#nextTrainInput').val().trim();
 
-	// Code for handling the push
-	dataRef.push({
-		train: train,
-		destination: destination,
-		militaryTime: militaryTime,
-		nextTrain: nextTrain
+		// Code for handling the push
+		dataRef.push({
+			train: train,
+			destination: destination,
+			militaryTime: militaryTime,
+			nextTrain: nextTrain
+		});
+
+		// Don't refresh the page!
+		return false;
 	});
 
-	// Don't refresh the page!
-	return false;
+
+	//Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+	dataRef.on("child_added", function(childSnapshot) {
+
+		// Log everything that's coming out of snapshot
+		console.log(childSnapshot.val().train);
+		console.log(childSnapshot.val().destination);
+		console.log(childSnapshot.val().militaryTime);
+		console.log(childSnapshot.val().nextTrain)
+
+
+	// Creating variables to append user info
+	var tName = childSnapshot.val().train;
+	var tDestination = childSnapshot.val().destination;
+	var tFrequency = childSnapshot.val().militaryTime;
+	var tNextTrain = childSnapshot.val().nextTrain;
+
+		// Appends user info to table
+		$(".table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tNextTrain + "</td>");
+
+		// Clears the HTML in bottom form
+		$("#trainInput").val("");
+		$("#destinationInput").val("");
+		$("#militaryTimeInput").val("");
+		$("#nextTrainInput").val("");
+
+		// ONLY WHEN I CLEAR FIREBASE.COM WILL THE TOP FORM EMPTY?!?!
+		// HOW DO I CLEAR THE HTML IN THE TOP FORM???
+		// $(".table > tbody").empty("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tNextTrain + "</td>");
+
+		// $("<td>").val("");
+		// $(tDestination).val("");
+		// $(tFrequency).val("");
+		// $(tNextTrain).val("");
+
+		// Handle the errors
+	},
+		function(errorObject){
+		console.log("Errors handled: " + errorObject.code);
+	});
+
+	dataRef.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+
+	});
 });
 
 
-//Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-dataRef.on("child_added", function(childSnapshot) {
 
-	// Log everything that's coming out of snapshot
-	console.log(childSnapshot.val().train);
-	console.log(childSnapshot.val().destination);
-	console.log(childSnapshot.val().militaryTime);
-	console.log(childSnapshot.val().nextTrain)
-
-
-// Creating variables to append user info
-var tName = childSnapshot.val().train;
-var tDestination = childSnapshot.val().destination;
-var tFrequency = childSnapshot.val().militaryTime;
-var tNextTrain = childSnapshot.val().nextTrain;
-
-// Appends user info to table
-$(".table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tNextTrain + "</td>");
-
-	// Clear the HTML
-	$("#newTrain").val("");
-	$("#destinationdisplay").val("");
-	$("#militaryTimedisplay").val("");
-	$("#nextTraindisplay").val("");
-
-
-	// Handle the errors
-},	function(errorObject){
-	console.log("Errors handled: " + errorObject.code)
-});
-
-dataRef.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-
-
-
-});
